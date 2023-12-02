9d8d9d9f6bef936d1b4075c25adf04c8010f63bd
Matthias Blank <Matthias.Blank2@gmx.net>
2023-12-02 10:35:59 +0100

    Formularanpassung

---
+++ src/app/addModel/services/model.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ContactFormModel } from '../contact-form-model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModelService {
+
+  private apiUrl = 'http://localhost:5000/save_model'; // URL to your Flask API
+
+  constructor(private http: HttpClient) { }
+
+  saveModelData(formData: ContactFormModel): Observable<any> {
+    return this.http.post(this.apiUrl, formData);
+  }
+}
